b535c35d4b4a286950cf9d3838acb797ea7f50dc
Evellyn Thamires <xe.uyemura@gmail.com>
2022-06-21 20:07:00 -0300

    Post, put and delete requests

--- tests/Api/api.spec.ts
+++ tests/Api/api.spec.ts
@@ -25,4 +25,63 @@ test.describe.parallel("API Testing", () => {
         expect(responseBody.data.last_name).toBe("Bluth")
         expect(responseBody.data.email).toBeTruthy()
     })
+
+    test("POST Request - Create New User", async ({ request }) => {
+        const response = await request.post(`${baseUrl}/users`, {
+            data: {
+                id: 1000
+            },
+        })
+
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(201)
+        expect(responseBody.id).toBe(1000)
+        expect(responseBody.createdAt).toBeTruthy()
+    })
+
+    test("POST Request - Login", async ({ request }) => {
+        const response = await request.post(`${baseUrl}/login`, {
+            data: {
+                email: "eve.holt@reqres.in",
+                password: "cityslicka"
+            },
+        })
+
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(200)
+        expect(responseBody.token).toBeTruthy()
+    })
+
+    test("POST Request - Login Fail", async ({ request }) => {
+        const response = await request.post(`${baseUrl}/login`, {
+            data: {
+                email: "eve.holt@reqres.in"
+            },
+        })
+        
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(400)
+        expect(responseBody.error).toBe("Missing password")
+    })
+
+    test("PUT Request - Update User", async ({ request }) => {
+        const response = await request.put(`${baseUrl}/users/2`, {
+            data: {
+                name: "New Name",
+                job: "New Job"
+            },
+        })
+
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(200)
+        expect(responseBody.name).toBe("New Name")
+        expect(responseBody.job).toBe("New Job")
+        expect(responseBody.updatedAt).toBeTruthy()
+    })
+
+    test("DELETE Request - Delete User", async ({ request }) => {
+        const response = await request.delete(`${baseUrl}/users/2`)
+
+        expect(response.status()).toBe(204)
+    })
 })
