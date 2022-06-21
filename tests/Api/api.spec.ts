f8a44e78aa2b6228157736cfa334a4ea6f710e05
Evellyn Thamires <xe.uyemura@gmail.com>
2022-06-20 21:48:58 -0300

    test get request

--- tests/Api/api.spec.ts
+++ tests/Api/api.spec.ts
@@ -14,4 +14,15 @@ test.describe.parallel("API Testing", () => {
         const response = await request.get(`${baseUrl}/users/non-existing-endpoint`)
         expect(response.status()).toBe(404)
     })
+
+    test("GET Request - Get User Detail", async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/1`)
+        const responseBody = JSON.parse(await response.text())
+
+        expect(response.status()).toBe(200)
+        expect(responseBody.data.id).toBe(1)
+        expect(responseBody.data.first_name).toBe("George")
+        expect(responseBody.data.last_name).toBe("Bluth")
+        expect(responseBody.data.email).toBeTruthy()
+    })
 })
